4cf8ccf9f3acc7228260b3b9d06068aad4f95011
Gabe Darley <gdarley@tulane.edu>
2021-11-05 13:41:07 -0500

    Added canned response improvements

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -35,7 +35,7 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "How's it going?"
+    let firstMessage = "Hi! How can we help you?"
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
     let time = getTime();
--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -1,10 +1,12 @@
 function getBotResponse(input) {
     // Simple responses
-    if (input == "hello") {
+    if (input == "hello" || input == "Hello" || input == "hi" || input == "Hi" ) {
         return "Hello there!";
-    } else if (input == "goodbye") {
-        return "Talk to you later!";
+    } else if (input == "goodbye" || input == "Goodbye" || input == "bye" || input == "Bye") {
+        return "Let us know if you need anything else!";
+    } else if (input == "Hi, I have a question!") {
+      return "What's the question? Maybe we can help!"
     } else {
-        return "Try asking something else!";
+        return "Try searching this question in the search bar to the left.";
     }
 }
