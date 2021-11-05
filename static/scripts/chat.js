606c61fcacca3c5d7de53a4d3920e7b2453c249b
Gabe Darley <gdarley@tulane.edu>
2021-11-05 13:02:38 -0500

    Finished tutorial

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -45,3 +45,48 @@ function firstBotMessage() {
 }
 
 firstBotMessage();
+
+function getHardResponse(userText) {
+  let botResponse = getBotResponse(userText);
+  let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+  $("#chatbox").append(botHtml);
+
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+function getResponse() {
+  let userText = $("#textInput").val();
+
+  if (userText == "") {
+    userText = "Hi, I have a question!";
+  }
+
+  let userHtml = '<p class="userText"><span>' + userText + '</span</p>';
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000)
+}
+
+function buttonSendText(sampleText) {
+  let userHtml = '<p class="userText"><span>' + sampleText + '</span</p>';
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+function sendButton() {
+  getResponse();
+}
+
+//Press enter to send message
+$("#textInput").keypress(function(e){
+  if(e.which == 13){
+    getResponse();
+  }
+})
