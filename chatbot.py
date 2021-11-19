2ffbcf0584f1fdba208da11badcc281ed0f83358
Dung Ngo <dungngotanlhp@gmail.com>
2021-11-19 14:43:23 -0600

    implement server & new models

---
+++ chatbot.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import math
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+def read_file(answer, question):
+    answers, questions = [], []
+
+    with open(answer, 'r') as file:
+        answers = file.readlines()
+
+    with open(question, 'r') as file:
+        questions = file.readlines()
+
+    return answers, questions
+
+
+sw = stopwords.words("english")
+
+
+def find_similarity(questions, user):
+    ranks = []
+
+    # tokenize the user's question
+    tokenized_user = word_tokenize(user)
+    user_set = {w for w in tokenized_user if not w in sw}
+
+    for idx, question in enumerate(questions):
+        l1, l2 = [], []
+
+        # tokenize the DB's question
+        tokenized_question = word_tokenize(question)
+        question_set = {w for w in tokenized_question if not w in sw}
+
+        rvector = question_set.union(user_set)
+        for w in rvector:
+            if w in user_set:
+                l1.append(1)
+            else:
+                l1.append(0)
+
+            if w in question_set:
+                l2.append(1)
+            else:
+                l2.append(0)
+
+        c = 0
+        # cosine formular
+        for i in range(len(rvector)):
+            c += l1[i]*l2[i]
+        cosine = c / ((sum(l1)**0.5*(sum(l2))**0.5))
+
+        # for each question, find its similarity to user's question
+        ranks.append((idx, cosine))
+
+    # sort the ranks
+    ranks.sort(key=lambda y: y[1], reverse=True)
+    return ranks
+
+
+def answer(ranks, answers):
+    f_idx, s_idx = ranks[0][0], ranks[1][0]
+    
+    print("The first answer is", answers[f_idx])
+    print("The second answer is", answers[s_idx])
+    
+    return answers[f_idx]
+
+if __name__ == "__main__":
+    answers, questions = read_file("data/answers.txt", "data/questions.txt")
+    ranks = find_similarity(questions, "Can you tell me what an IEP is")
+    answer(ranks, answers)
--- main.py
+++ main.py
@@ -1,60 +1,22 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import numpy as np
-import math
+from flask import Flask
+from flask import request
+from chatbot import read_file, find_similarity, answer
 
-print("Hi, how can we help you?")
-x = input()
-
-sw = stopwords.words('english')
-# print(sw)
+app = Flask(__name__)
+answers, questions = read_file("data/answers.txt", "data/questions.txt")
 
 
-question_vectors = []
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-ranks = []
-for idx, question in enumerate(questions):
-    l1 = []
-    l2 = []
-    temp = word_tokenize(question)
-    question_vectors.append(temp)
 
-    user = word_tokenize(x)
+@app.route("/answer", methods=['POST'])
+def answer_endpoint():
+    message = request.form.get('message')
+    ranks = find_similarity(questions, message)
+    return answer(ranks, answers)
 
-    # remove stop words from the string
-    temp_set = {w for w in temp}  # if not w in sw}
-    print(temp_set)
-    user_set = {w for w in user}  # if not w in sw}
-    print(user_set)
 
-    # form a set containing keywords of both strings
-    rvector = temp_set.union(user_set)
-    for w in rvector:
-        if w in user_set:
-            l1.append(1)  # create a vector
-        else:
-            l1.append(0)
-        if w in temp_set:
-            l2.append(1)
-        else:
-            l2.append(0)
-    c = 0
-
-    # cosine formula
-    for i in range(len(rvector)):
-        c += l1[i]*l2[i]
-    cosine = c / ((sum(l1)**0.5*(sum(l2))**0.5))
-    print("similarity: ", cosine)
-    ranks.append((idx, cosine))
-
-ranks.sort(key=lambda y: y[1], reverse=True)
-print()
-print(ranks)
-print()
-print("==Answer One:==")
-print(answers[ranks[0][0]])
-print()
-print("==Answer Two:==")
-print(answers[ranks[1][0]])
+if __name__ == '__main__':
+    app.run(debug=True)
