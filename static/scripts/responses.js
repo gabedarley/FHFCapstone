9d171d47c00bf80da39d9c836d119133b546c67e
gabedarley <gdarley@tulane.edu>
2022-03-07 01:22:03 -0600

    Minor CSS update

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -1,11 +1,11 @@
 async function getBotResponse(input) {
   // Simple responses
   if (input == "hello" || input == "Hello" || input == "hi" || input == "Hi" || input == "test") {
-    return "Hello there!";
+    return {"answer": ["Hello there!"], "source":""};
   } else if (input == "goodbye" || input == "Goodbye" || input == "bye" || input == "Bye") {
-    return "Let us know if you need anything else!";
+    return {"answer": ["Let us know if you need anything else!"], "source":""};
   } else if (input == "Hi, I have a question!") {
-    return "What's the question? Maybe we can help!"
+    return {"answer": ["What's the question? Maybe we can help!"], "source":""};
   } else {
     let url = 'http://127.0.0.1:5000/answer';
     let data = { "message": input };
